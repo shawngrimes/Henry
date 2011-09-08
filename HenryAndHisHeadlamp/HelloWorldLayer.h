bd5e11ec42a0360483324f15e50f8dec11430925
Shawn Grimes <shawn@shawnsbits.com>
2011-09-08 13:28:43 -0400

    Initial Commit

---
+++ HenryAndHisHeadlamp/HelloWorldLayer.h
@@ -0,0 +1,21 @@
+//
+//  HelloWorldLayer.h
+//  HenryAndHisHeadlamp
+//
+//  Created by Shawn Grimes on 9/8/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
